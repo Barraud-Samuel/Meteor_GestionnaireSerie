73ae902364429bbc7cad3ca71cc65ed006728ffa
Barraud-Samuel <samuel.barraud@hetic.net>
2017-11-21 12:21:24 +0100

    adding the input and the bdd

--- Gestionnaire_Serie/imports/ui/body.js
+++ Gestionnaire_Serie/imports/ui/body.js
@@ -1,11 +1,32 @@
 import { Template } from 'meteor/templating';
  
+import { Series } from '../api/series.js';
+ 
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  series() {
+    // Show newest series at the top
+    return Series.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-series'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;	
+    const text = target.text.value;
+ 
+    // Insert a series into the collection
+    Series.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
+  },
 });
