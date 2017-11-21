bed1b283f6a20623e137d976935f73497b3d8536
Barraud-Samuel <samuel.barraud@hetic.net>
2017-11-21 14:53:07 +0100

    implementing incomplete task

--- Gestionnaire_Serie/imports/ui/body.js
+++ Gestionnaire_Serie/imports/ui/body.js
@@ -1,14 +1,29 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
  
 import { Series } from '../api/series.js';
  
 import './serie.js'; 
 import './body.html';
  
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   series() {
+  	const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Series.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     // Show newest series at the top
     return Series.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Series.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -29,5 +44,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
