10adbdddd38dfbd9a3badbb5961bdb77026efe60
Barraud-Samuel <samuel.barraud@hetic.net>
2017-12-12 12:38:02 +0100

    adding login route

--- eleve_Gest/imports/startup/client/routes.js
+++ eleve_Gest/imports/startup/client/routes.js
@@ -1,23 +1,29 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Meteor } from 'meteor/meteor';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/login/login.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
+    if (Meteor.userId()){
+        BlazeLayout.render('App_body', { main: 'App_home' });
+    }else{
+        FlowRouter.go("/Login");
+        console.log('log');
+    }
   },
 });
 
 FlowRouter.route('/Login', {
-    name: 'App.home',
     action() {
-        BlazeLayout.render('App_body', { main: 'App_home' });
+        BlazeLayout.render('App_body', { main: 'App_login' });
     },
 });
 
---
+++ eleve_Gest/imports/ui/pages/login/login.js
@@ -0,0 +1,4 @@
+/**
+ * Created by samuel on 12/12/17.
+ */
+import './login.html';
