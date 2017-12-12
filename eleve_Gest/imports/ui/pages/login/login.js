3bdf65149d1d768399b2ed600f062f03e5975224
Barraud-Samuel <samuel.barraud@hetic.net>
2017-12-12 15:05:45 +0100

    adding login connection

--- eleve_Gest/imports/ui/pages/login/login.js
+++ eleve_Gest/imports/ui/pages/login/login.js
@@ -1,4 +1,27 @@
 /**
  * Created by samuel on 12/12/17.
  */
+import { Eleves } from '/imports/api/links/eleves.js';
+import { Meteor } from 'meteor/meteor';
 import './login.html';
+
+
+
+//template log out (remplacer le nom du template)
+Template.App_login.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        //FlowRouter.go("/Login");
+    }
+});
+
+Template.App_login.events({
+    'submit .login'(event){
+        event.preventDefault();
+        const target = event.target;
+        const email = target.email.value;
+        const password = target.password.value;
+        Meteor.loginWithPassword(email, password);
+    },
+});
