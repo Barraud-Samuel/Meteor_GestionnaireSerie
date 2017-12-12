61bc54ddfe41ecf02489655170b9203cdea0e8b7
Barraud-Samuel <samuel.barraud@hetic.net>
2017-12-12 17:12:16 +0100

    adding some front tamplete (not created yet)

--- eleve_Gest/imports/ui/pages/login/login.js
+++ eleve_Gest/imports/ui/pages/login/login.js
@@ -2,18 +2,20 @@
  * Created by samuel on 12/12/17.
  */
 import { Eleves } from '/imports/api/links/eleves.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Accounts } from 'meteor/accounts-base'
 import { Meteor } from 'meteor/meteor';
 import './login.html';
 
 
-
 //template log out (remplacer le nom du template)
 Template.App_login.events({
-    'click .logout': function(event){
+    'click .logout'(event){
         event.preventDefault();
         Meteor.logout();
-        //FlowRouter.go("/Login");
-    }
+        FlowRouter.go("/Login");
+    },
 });
 
 Template.App_login.events({
@@ -22,6 +24,12 @@ Template.App_login.events({
         const target = event.target;
         const email = target.email.value;
         const password = target.password.value;
-        Meteor.loginWithPassword(email, password);
+        Meteor.loginWithPassword(email, password, function(error){
+            if(error){
+                console.log(error.reason);
+            } else {
+                FlowRouter.go("/");
+            }
+        });
     },
 });
