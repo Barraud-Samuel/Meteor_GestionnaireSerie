b84eb3508718ae1e186c4c658763adde0cf08f4e
Barraud-Samuel <samuel.barraud@hetic.net>
2017-12-16 23:34:33 +0100

    add user and admin roles who depends from the email and the admin route

--- projet_testGS/GS_test/imports/startup/client/routes.js
+++ projet_testGS/GS_test/imports/startup/client/routes.js
@@ -1,17 +1,55 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Meteor } from 'meteor/meteor';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/login/login.js';
+import '../../ui/pages/admin/admin.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
-  name: 'App.home',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
-  },
+    name: 'App.home',
+    action() {
+        if (Meteor.userId()){
+            adminOruserRoute();
+        }else{
+            FlowRouter.go("/Login");
+            console.log('log');
+        }
+    },
+});
+
+//Admin condition function
+function adminOruserRoute() {
+    Meteor.call('admin_route', function (error, result) {
+        if(error){
+            console.log(error);
+        } else {
+            console.log(result);
+            if (result === true){
+                console.log('admin');
+                BlazeLayout.render('App_body',{main: 'App_admin'});
+            }else{
+                console.log('user');
+                BlazeLayout.render('App_body', { main: 'App_home' });
+            }
+        }
+    });
+}
+//login route
+FlowRouter.route('/Login', {
+    action() {
+        BlazeLayout.render('App_body', { main: 'App_login' });
+    },
+});
+
+FlowRouter.route('/admin',{
+    action(){
+        BlazeLayout.render('App_body', {main: 'App_admin'});
+    }
 });
 
 //404 error
@@ -35,25 +73,4 @@ FlowRouter.route('/login', {
     action() {
         BlazeLayout.render('App_body', { main: 'App_home' });
     },
-});*/
-
-
-//routes administrateurs
-/*
-var adminRoutes = FlowRouter.group({
-    prefix: '/admin',
-    name: 'admin',
-    triggersEnter: [function(context, redirect) {
-        console.log('running group triggers');
-    }]
-});
-
-// handling /admin route
-adminRoutes.route('/', {
-    action: function() {
-        BlazeLayout.render('componentLayout', {content: 'admin'});
-    },
-    triggersEnter: [function(context, redirect) {
-        console.log('running /admin trigger');
-    }]
 });*/
---
+++ projet_testGS/GS_test/imports/ui/pages/admin/admin.js
@@ -0,0 +1,4 @@
+/**
+ * Created by samuel on 16/12/17.
+ */
+import './admin.html'
